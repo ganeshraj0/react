155ad6a2cf5db581318593730c0bce18085c1f79
ganeshraj0 <ganesh636164@gmail.com>
2024-07-05 05:14:43 -0700

    usestate comple

--- src/Contaner.js
+++ src/Contaner.js
@@ -1,35 +1,17 @@
 import React, { useState } from 'react'
 
 const Contaner = () => {
+    const [name ,setName] = useState("Sarath")
     function changeName()
     {
         const names =["sarath","chandru","suri"]
         const num =Math.floor(Math.random()*3)
-        return names[num]
+        setName(names[num])
     }
-    function changeName2(e)
-    {
-        alert(e.target.textContent)
-    }
-    let [count , setCount] = useState(99)
-
-    function increment ()
-    {
-        setCount (count  +1)
-    }
-
-    function decrement ()
-    {
-        setCount(count-1)
-    }
-
   return (
     <main>
-    <h2>My Friend Is {() => changeName () }</h2>
-    <button onClick={(e) => changeName2(e)}>Change Name</button>
-    <button onClick={()=>decrement()}>-</button>
-    <span>{count}</span>
-    <button onClick={()=> increment ()}>+</button>
+    <h2>My Friend Is {name} </h2>
+    <button onClick={() => changeName()}>Change Name</button>
     </main>
   )
 }
